1798914cd40d2c0cdc7c10da168bd38fd367f5ec
dair68 <granthuang68@yahoo.com>
2019-08-28 12:18:05 -0700

    removed controller file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,41 +1,43 @@
 var express = require('express');
-var controller = require('../controller');
+var petModel = require('../models/index');
+var handleError = require('./handleError');
+
 var router = express.Router();
 
-console.log("obtaining routes");
-
-router.get('/', function (req, res) {
-    res.send('Welcome to the Petting Zoo!');
-});
+router.get('/', (req, res) => res.send('Welcome to the Petting Zoo!'));
 
 router.get('/api/pets', function (req, res) {
-    controller.getAllPets(req.body, null, function (err, docs) {
+    petModel.find(function (err, docs) {
         if (err) {
-            console.log(err);
+            handleError(res, err.message, 'could not obtain pets');
         }
 
         res.send(docs);
     });
-})
+});
 
 router.get('/api/pets/:numPets', function (req, res) {
-    controller.getPets(req.body, null, +(req.params.numPets), function (err, docs) {
+    var numPets = +(req.params.numPets);
+    
+    petModel.find({}, null, {limit: numPets}, function (err, docs) {
         if (err) {
-            console.log(err);
+            handleError(res, err.message, 'could not obtain pets');
         }
 
         res.send(docs);
     });
-})
+});
 
 router.get('/api/pet-details/:id', function (req, res) {
-    controller.getPetDetails(req.params.id, function(err, pet) {
-        if(err) {
-            console.log(err);
+    var id = req.params.id;
+    
+    petModel.findById(id, function (err, pet) {
+        if (err) {
+            handleError(res, err.message, 'could not find pet-details');
         }
 
         res.send(pet);
     });
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,22 +3,24 @@ const app = express();
 const port = 3000;
 
 var mongoose = require('mongoose');
+var mongoURL = 'mongodb://localhost:27017/petstore';
+
 var router = require('./backend/routes');
+var bodyParser = require('body-parser');
+
+//connecting to database
+mongoose.connect(mongoURL, { useNewUrlParser: true }, err => console.log(err.message));
+
+//if error occurs after database connection
+mongoose.connection.on('error', err => console.log(err.message));
 
 //middleware
 app.use(router);
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-mongoose.connect('mongodb://localhost:27017/petstore', { useNewUrlParser: true }, function (err) {
-    if (err) {
-        handleError(error);
-    }
-
-    app.listen(port, function () { console.log(`app listening on port ${port}`) });
-});
-
-mongoose.connection.on('error', err => {
-    logError(err);
-});
+//connecting app to server
+app.listen(port, () => console.log(`app listening on port ${port}`));
 
 
 
