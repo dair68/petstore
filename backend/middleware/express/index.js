2233b6ef5f41f08e0aa67c779a760cebca88c6ca
dair68 <granthuang68@yahoo.com>
2019-09-12 01:19:38 -0700

    changed location of dist static files

--- backend/middleware/express/index.js
+++ backend/middleware/express/index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 module.exports = (app, distDir) => {
+    // Creating link to Angular build directory
+    app.use(express.static(distDir));
+
     //adding middleware
     app.use(bodyParser.json()) // for parsing application/json
     app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
     app.use(router);
-
-    // Creating link to Angular build directory
-    app.use(express.static(distDir));
 }
